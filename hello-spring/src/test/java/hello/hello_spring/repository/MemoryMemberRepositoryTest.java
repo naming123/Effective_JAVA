6140325326b3aceb74ae6043840e6883c4998284
naming123 <osm1307@gmail.com>
2025-11-15 16:11:29 +0900

    멤버 "spring"회원테스트 만들기

--- hello-spring/src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -1,11 +1,20 @@
 package hello.hello_spring.repository;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import hello.hello_spring.domain.Member;
 import org.junit.jupiter.api.Assertions;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class MemoryMemberRepositoryTest {
     MemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+
+    }
 
     @Test
     public void save(){
@@ -16,7 +25,36 @@ class MemoryMemberRepositoryTest {
 
         Member result = repository.findById(member.getId()).get();
 //        System.out.println("result = " + (result == null));
-        Assertions.assertEquals(result, member);
+        assertThat(member).isEqualTo(result);
+    }
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+
+    }
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(3);
     }
 
 }
